f2bae73ccbe792fd92ea59bc9c918600a399daae
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-16 22:39:05 +0200

    qoutes

--- commands/quote.js
+++ commands/quote.js
@@ -1,17 +1,36 @@
+const { MessageFlags } = require("discord.js");
+
 module.exports = {
     name: "quote",
     aliases: ["q"],
     description: "quote something!",
     cooldown: 5,
     guildOnly: true,
-    args: true,
+    args: false,
     usage: false,
     // eslint-disable-next-line no-unused-vars
     execute(msg, args){
         const fetch = require("node-fetch")
         let author = args.join(" ");
-        console.log((`https://api.quotable.io/random${author ?`?author=${author}` : ""}`))
-        fetch((`https://api.quotable.io/random${author ?`?author=${author}` : ""}`))
+        let fetchAddress = "";
+        if(author == ""){
+            fetchAddress = `https://api.quotable.io/random"`
+        }
+        else{
+            fetch(`https://api.quotable.io/authors?slug=${author}`)
+            .then(response => {
+            return response.json();
+            })
+            .then( (result) => {
+            if(result.count > 0){
+                fetchAddress = `https://api.quotable.io/random${author ?`?author=${author}` : ""}`
+            }
+            else{
+                msg.reply("This author is not found, but, there is some random quote: \n");
+            }  
+        })
+        }
+        fetch(fetchAddress)
         .then(response => {
             return response.json();
         })
